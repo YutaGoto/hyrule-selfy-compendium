dd639b35db98bb94265248270b36a38576a322ae
YutaGoto <you.goto.510@gmail.com>
2023-05-14 13:35:26 +0900

    :mag: fix meta tags

--- app/meta.tsx
+++ app/meta.tsx
@@ -1,19 +1,30 @@
 import { headers } from 'next/headers';
+import { items } from '@/lib/items';
 
-export const Meta = () => {
+interface MetaProps {
+  id?: string;
+}
+
+export const Meta = ({ id }: MetaProps) => {
+  const item = items.find((x) => x.id === id);
+  const filePath = id ? `/assets/images/${id}.jpg` : '/og-image.jpg';
+  const title = item
+    ? `${item.name} - 自撮りハイラル図鑑`
+    : '自撮りハイラル図鑑';
+
   const headersInstance = headers();
   return (
     <head>
-      <title>自撮りハイラル図鑑</title>
+      <title>{title}</title>
 
       <meta
         property="og:image"
-        content={`https://${headersInstance.get('host')}/og-image.jpg`}
+        content={`https://${headersInstance.get('host')}${filePath}`}
       />
       <meta property="og:image:type" content="image/jpg" />
       <meta
         name="twitter:image"
-        content={`https://${headersInstance.get('host')}/og-image.jpg`}
+        content={`https://${headersInstance.get('host')}${filePath}`}
       />
       <meta name="twitter:image:type" content="image/jpg" />
       <meta name="twitter:card" content="summary_large_image" />
