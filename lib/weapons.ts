520fdcbea3eae1e45205b5f30b9f6d1a7279c873
YutaGoto <you.goto.510@gmail.com>
2023-08-20 15:25:00 +0900

    :bento: add spears

--- lib/weapons.ts
+++ lib/weapons.ts
@@ -858,29 +858,32 @@ export const weapons: Item[] = [
     id: '277',
     category: 'weapons',
     name: 'トゲボコバット',
-    description: 'ボコブリンが攻撃力を高める工夫として動物の骨で作ったとげを付け足したボコバット。実際直撃すると結構なダメージを受ける。',
+    description:
+      'ボコブリンが攻撃力を高める工夫として動物の骨で作ったとげを付け足したボコバット。実際直撃すると結構なダメージを受ける。',
     locations: ['フィローネ草原', '東ハテール'],
     properties: {
       type: 'sword',
       value: 18,
-    }
+    },
   },
   {
     id: '278',
     category: 'weapons',
     name: '竜骨ボコバット',
-    description: 'ボコバットに化石の骨を組み合わせたこん棒。強敵を殴り倒すのに十分な強度と攻撃力を持つ。ボコブリンの中でも一部の猛者だけが手にする。',
+    description:
+      'ボコバットに化石の骨を組み合わせたこん棒。強敵を殴り倒すのに十分な強度と攻撃力を持つ。ボコブリンの中でも一部の猛者だけが手にする。',
     locations: ['ハイラル丘陵', 'ハテール海'],
     properties: {
       type: 'sword',
       value: 36,
-    }
+    },
   },
   {
     id: '279',
     category: 'weapons',
     name: 'モリブリンバット',
-    description: 'モリブリンが好んで使う簡素なこん棒。硬い木で作られているが雑な作りなため、すぐに壊れてしまう。',
+    description:
+      'モリブリンが好んで使う簡素なこん棒。硬い木で作られているが雑な作りなため、すぐに壊れてしまう。',
     locations: ['ゲルド高地', 'フィローネ草原'],
     properties: {
       type: 'sword',
@@ -891,121 +894,576 @@ export const weapons: Item[] = [
     id: '280',
     category: 'weapons',
     name: 'トゲモリブリンバット',
-    description: '改良されたモリブリンの両手武器。殴りつける面に動物の骨がくくりつけられており、相手に与えるダメージが格段に高まった。',
+    description:
+      '改良されたモリブリンの両手武器。殴りつける面に動物の骨がくくりつけられており、相手に与えるダメージが格段に高まった。',
     locations: ['ハイラル平原', 'アッカレ高原'],
     properties: {
       type: 'sword',
       value: 27,
-    }
+    },
   },
   {
     id: '281',
     category: 'weapons',
     name: '竜骨モリブリンバット',
-    description: '太古の獣の骨を取り付けたモリブリンバット。斧のように先端に重心がある作りとなっており、これを扱えるモリブリンは戦い慣れていて危険。',
+    description:
+      '太古の獣の骨を取り付けたモリブリンバット。斧のように先端に重心がある作りとなっており、これを扱えるモリブリンは戦い慣れていて危険。',
     locations: ['へブラ山脈', 'オルディン山脈'],
     properties: {
       type: 'sword',
       value: 45,
-    }
+    },
   },
   {
     id: '282',
     category: 'weapons',
     name: 'ガーディアンアクス',
-    description: '小型ガーディアンが装備する武器。幅広で薄い刀身は古代の力によって作られており、高い攻撃力を持つが構造的に壊れやすい。',
+    description:
+      '小型ガーディアンが装備する武器。幅広で薄い刀身は古代の力によって作られており、高い攻撃力を持つが構造的に壊れやすい。',
     locations: ['ゲルド砂漠', 'ハイラル平原'],
     properties: {
       type: 'sword',
       value: 30,
-    }
+    },
   },
   {
     id: '283',
     category: 'weapons',
     name: 'ガーディアンアクス＋',
-    description: '出力強化されたガーディアンアクス。子の武器には戦闘だけでなく、障害物を焼き切り進路を確保する用途があったと考えられている。',
+    description:
+      '出力強化されたガーディアンアクス。子の武器には戦闘だけでなく、障害物を焼き切り進路を確保する用途があったと考えられている。',
     locations: ['アッカレ高原', 'ハイラル平原'],
     properties: {
       type: 'sword',
       value: 45,
-    }
+    },
   },
   {
     id: '284',
     category: 'weapons',
     name: 'ガーディアンアクス++',
-    description: '敵を一撃で両断するために、最大限まで出力が強化されたガーディアンアクス。金属製の武器では出しにくい高い切断力を古代技術が実現している。',
+    description:
+      '敵を一撃で両断するために、最大限まで出力が強化されたガーディアンアクス。金属製の武器では出しにくい高い切断力を古代技術が実現している。',
     locations: ['へブラ山脈', 'ゲルド高地'],
     properties: {
       type: 'sword',
       value: 60,
-    }
+    },
   },
   {
     id: '285',
     category: 'weapons',
     name: 'ライネルの大剣',
-    description: 'ライネルが携える両手武器。剣というよりただの金属の塊といったほうが良いが、ライネルの力で振られると大抵のものを粉砕する凶器となる。',
+    description:
+      'ライネルが携える両手武器。剣というよりただの金属の塊といったほうが良いが、ライネルの力で振られると大抵のものを粉砕する凶器となる。',
     locations: ['ゲルド高地', 'タバンタ辺境'],
     properties: {
       type: 'sword',
       value: 36,
-    }
+    },
   },
   {
     id: '286',
     category: 'weapons',
     name: '獣王の大剣',
-    description: '硬度の高い金属が先端に取り付けられ、打撃力と耐久性が増したライネルの両手武器。圧倒的な重量で盾や鎧ごと敵を叩き潰す。',
+    description:
+      '硬度の高い金属が先端に取り付けられ、打撃力と耐久性が増したライネルの両手武器。圧倒的な重量で盾や鎧ごと敵を叩き潰す。',
     locations: ['ゲルド高地', 'へブラ山脈'],
     properties: {
       type: 'sword',
       value: 54,
-    }
+    },
   },
   {
     id: '287',
     category: 'weapons',
     name: '獣神の大剣',
-    description: 'デスマウンテン山頂から取れる特殊な金属を使うことによって桁外れな重さを持った両手武器。振り降ろされる一撃は比類なき破壊力を誇る。',
+    description:
+      'デスマウンテン山頂から取れる特殊な金属を使うことによって桁外れな重さを持った両手武器。振り降ろされる一撃は比類なき破壊力を誇る。',
     locations: ['へブラ山脈', 'オルディン山脈'],
     properties: {
       type: 'sword',
       value: 78,
-    }
+    },
   },
   {
     id: '288',
     category: 'weapons',
     name: '風斬り刀',
-    description: 'イーガ団の幹部が携える風変りな刀。熟練した使い手が精神統一して振り抜くと、その特殊な形状によって真空の刃が作り出される。',
+    description:
+      'イーガ団の幹部が携える風変りな刀。熟練した使い手が精神統一して振り抜くと、その特殊な形状によって真空の刃が作り出される。',
     locations: ['ゲルド高地', '西ハテール'],
     properties: {
       type: 'sword',
       value: 40,
-    }
+    },
   },
   {
     id: '289',
     category: 'weapons',
     name: 'モリブリンの骨',
-    description: '腕だけになっても動き続けるモリブリンの骨。長くて太い骨は武器として使うに十分な打撃力を持つが、非常にもろく壊れやすい。',
+    description:
+      '腕だけになっても動き続けるモリブリンの骨。長くて太い骨は武器として使うに十分な打撃力を持つが、非常にもろく壊れやすい。',
     locations: ['ハイラル全土'],
     properties: {
       type: 'sword',
       value: 15,
-    }
+    },
   },
   {
     id: '290',
     category: 'weapons',
     name: '木のモップ',
-    description: '床の清掃に大活躍する掃除道具。職人による手作りのため丈夫にできている。武器としてもある程度使える。',
+    description:
+      '床の清掃に大活躍する掃除道具。職人による手作りのため丈夫にできている。武器としてもある程度使える。',
     locations: ['東ハテール', 'ハイラル平原'],
     properties: {
       type: 'sword',
       value: 5,
-    }
-  }
+    },
+  },
+  {
+    id: '291',
+    category: 'weapons',
+    name: '畑のフォーク',
+    description:
+      '干し草を効率よく集めるための農具。誰でも扱えるように軽く作られている。4つに分かれた先端はけっこう鋭い。',
+    locations: ['東ハテール', '西ハテール'],
+    properties: {
+      type: 'sword',
+      value: 7,
+    },
+  },
+  {
+    id: '292',
+    category: 'weapons',
+    name: '漁師のモリ',
+    description:
+      '主に大型の魚を獲るために使う漁具。鋭くとがった先端を持ち武器としても使える。逃げる魚をこれで仕留められれば、一人前の漁師。',
+    locations: ['東ハテール', 'ハイリア湖'],
+    properties: {
+      type: 'sword',
+      value: 8,
+    },
+  },
+  {
+    id: '293',
+    category: 'weapons',
+    name: '投てき槍',
+    description:
+      '投げて突き刺す事に特化した特殊な槍。穂先と柄のバランスを取る事で、通常の槍より遠くまで投げられるように設計されている。',
+    locations: ['西ハテール', 'ハイラル平原'],
+    properties: {
+      type: 'sword',
+      value: 6,
+    },
+  },
+  {
+    id: '294',
+    category: 'weapons',
+    name: '旅人の槍',
+    description:
+      '旅人がオオカミなどの野生動物から身を守るために持ち歩く護身用の槍。握りやすく使い心地が良い。',
+    locations: ['ハイラル平原', '西ハテール'],
+    properties: {
+      type: 'sword',
+      value: 3,
+    },
+  },
+  {
+    id: '295',
+    category: 'weapons',
+    name: '兵士の槍',
+    description:
+      '城の兵士が使っていた長槍。思い通りに振り回すには十分な鍛錬が必要。鉄の穂先は頑丈で火が付いても燃えない。',
+    locations: ['ハイラル平原', 'フィローネ草原'],
+    properties: {
+      type: 'sword',
+      value: 7,
+    },
+  },
+  {
+    id: '296',
+    category: 'weapons',
+    name: '騎士の槍',
+    description:
+      '武器に熟練した騎士のために作られた槍。斧のような刃が付いた槍頭が特徴的。馬上で扱うことも多かったと言われる。',
+    locations: ['タバンタ辺境', 'ゲルド砂漠'],
+    properties: {
+      type: 'sword',
+      value: 13,
+    },
+  },
+  {
+    id: '297',
+    category: 'weapons',
+    name: '王家の槍',
+    description:
+      '謁見の間など城の中でも特に重要な場所を守る任務に就いていた騎士に与えられた槍。王家付きの職人による美しい装飾が施されている。',
+    locations: ['ゲルド高地', 'タバンタ辺境'],
+    properties: {
+      type: 'sword',
+      value: 26,
+    },
+  },
+  {
+    id: '298',
+    category: 'weapons',
+    name: '森人の槍',
+    description:
+      'コログ族が人間のために作ったといわれる槍。軽くて丈夫な木を材料にしており扱いやすい。穂先は特に硬い木で削って作られている。',
+    locations: ['ハイラル大森林', 'ハイラル丘陵'],
+    properties: {
+      type: 'sword',
+      value: 11,
+    },
+  },
+  {
+    id: '299',
+    category: 'weapons',
+    name: 'ゾーラの槍',
+    description:
+      'ゾーラ族に広く一般的に使われている槍。漁だけでなく外敵から里を守る戦いにも使われる。特殊な金属で作られているので見た目よりも軽い。',
+    locations: ['ラネール大水源', 'ハイリア湖'],
+    properties: {
+      type: 'sword',
+      value: 9,
+    },
+  },
+  {
+    id: '300',
+    category: 'weapons',
+    name: '銀鱗の槍',
+    description:
+      'ゾーラの中でも戦いを得意とする者が扱う槍。魚の尾のような美しい意匠の刃を持ち、その切っ先は硬い鱗も貫通するほどに鋭い。',
+    locations: ['アッカレ高原', 'ラネール大水源'],
+    properties: {
+      type: 'sword',
+      value: 12,
+    },
+  },
+  {
+    id: '301',
+    category: 'weapons',
+    name: '祭事の槍',
+    description:
+      'ミファーが用いた光鱗の槍を模して作られた槍。見た目は光鱗の槍とそっくりだが、攻撃性能や耐久性はやや劣る。',
+    locations: ['ラネール大水源'],
+    properties: {
+      type: 'sword',
+      value: 14,
+    },
+  },
+  {
+    id: '302',
+    category: 'weapons',
+    name: '光鱗の槍',
+    description:
+      '英傑ミファーが愛用していた優美な名槍。治癒を得意とするミファーだったが、槍を扱いも一流であったと言われている。',
+    locations: ['ラネール大水源'],
+    properties: {
+      type: 'sword',
+      value: 22,
+    },
+  },
+  {
+    id: '303',
+    category: 'weapons',
+    name: '削岩棒',
+    description:
+      'ゴロン族が作った金属製の部品を転用して作られた武器。先端に掘削用の部品が使われており、硬い鉱石も簡単に砕くことができる。',
+    locations: ['オルディン峡谷', 'へブラ山脈'],
+    properties: {
+      type: 'sword',
+      value: 14,
+    },
+  },
+  {
+    id: '304',
+    category: 'weapons',
+    name: '風切羽の槍',
+    description:
+      'リト族の兵士が使う槍。空を羽ばたきながら使うことができるように、軽くて丈夫な材料を使って作られている。',
+    locations: ['タバンタ辺境', 'ハイラル平原'],
+    properties: {
+      type: 'sword',
+      value: 10,
+    },
+  },
+  {
+    id: '305',
+    category: 'weapons',
+    name: 'ゲルドの槍',
+    description:
+      'ゲルド族の兵士に広く用いられている槍。槍頭に重量があるため扱いが難しいが、長身なゲルド族が使うと頼もしい強さを発揮する。',
+    locations: ['ゲルド高地', 'ゲルド砂漠'],
+    properties: {
+      type: 'sword',
+      value: 16,
+    },
+  },
+  {
+    id: '306',
+    category: 'weapons',
+    name: '散打の槍',
+    description:
+      '独特な形状の槍頭を持つ槍。シーカー族の槍の達人は湾曲した刃を引っかけて斬る使い方をしたとも言われる。',
+    locations: ['西ハテール', 'ハイラル平原'],
+    properties: {
+      type: 'sword',
+      value: 12,
+    },
+  },
+  {
+    id: '307',
+    category: 'weapons',
+    name: '古代兵装・槍',
+    description:
+      'ガーディアンに使われている古代技術の研究成果を利用して作られた槍。穂先の青い刃は高い貫通力を持つ。',
+    locations: ['不明'],
+    properties: {
+      type: 'sword',
+      value: 30,
+    },
+  },
+  {
+    id: '308',
+    category: 'weapons',
+    name: '錆びた槍',
+    description:
+      '大昔に騎士が使っていたと思われる錆びついた槍。長く風雨に晒されたせいか、槍頭の痛みが激しく耐久性は低い。',
+    locations: ['ハイラル平原', 'ハイラル大森林'],
+    properties: {
+      type: 'sword',
+      value: 5,
+    },
+  },
+  {
+    id: '309',
+    category: 'weapons',
+    name: '近衛の槍',
+    description:
+      'シーカー族が厄災への対策として古代技術を活かして作った槍。非常に高い攻撃力を持つが、耐久性が低く実際の戦いには使われなかった。',
+    locations: ['不明'],
+    properties: {
+      type: 'sword',
+      value: 32,
+    },
+  },
+  {
+    id: '310',
+    category: 'weapons',
+    name: '火炎の槍',
+    description:
+      'デスマウンテンの溶岩を特殊な技術で精錬して作られたと言われる魔法の槍。赤く輝いているときは突きと同時に炎を発する。',
+    locations: ['ハイラル平原', 'へブラ山脈'],
+    properties: {
+      type: 'sword',
+      value: 24,
+    },
+  },
+  {
+    id: '311',
+    category: 'weapons',
+    name: '氷雪の槍',
+    description:
+      'へブラ山脈に眠る太古の氷を使って鍛錬したと言われる魔法の槍。穂先が青白く輝いている時に突くと、冷気を放ち相手を凍りつかせる。',
+    locations: ['へブラ山脈', 'ハイラル平原'],
+    properties: {
+      type: 'sword',
+      value: 20,
+    },
+  },
+  {
+    id: '312',
+    category: 'weapons',
+    name: '雷電の槍',
+    description:
+      '雷の大地の落雷をその穂先に与え続けて作られたと言われる魔法の槍。金色の光が満ちている時に突くと雷撃を放つ。',
+    locations: ['ハイラル丘陵', 'ハイラル平原'],
+    properties: {
+      type: 'sword',
+      value: 22,
+    },
+  },
+  {
+    id: '313',
+    category: 'weapons',
+    name: 'ボコ槍',
+    description:
+      '太い木の枝から乱暴に削りだされた無骨な槍。肉や魚を刺して火をあぶるのにも使うようだ。',
+    locations: ['西ハテール', 'ハイラル平原'],
+    properties: {
+      type: 'sword',
+      value: 2,
+    },
+  },
+  {
+    id: '314',
+    category: 'weapons',
+    name: 'トゲボコ槍',
+    description:
+      '鋭い動物の骨を槍頭として使ったボコブリンの槍。金属の穂先を持つ槍より軽量で見かけより扱いやすい。',
+    locations: ['フィローネ草原', '東ハテール'],
+    properties: {
+      type: 'sword',
+      value: 6,
+    },
+  },
+  {
+    id: '315',
+    category: 'weapons',
+    name: '竜骨ボコ槍',
+    description:
+      '硬い化石の骨を穂先にくくりつけた槍。化石にある牙が敵に食い込む作りになっている。この槍を装備しているボコブリンには注意が必要。',
+    locations: ['ハテール海', 'ハイラル丘陵'],
+    properties: {
+      type: 'sword',
+      value: 12,
+    },
+  },
+  {
+    id: '316',
+    category: 'weapons',
+    name: 'モリブリン槍',
+    description:
+      'モリブリンが使う木製の槍。槍頭は森の木を荒っぽく削って尖らせただけなので、突き刺す力も耐久性も高くはない。',
+    locations: ['フィローネ草原', 'ゲルド高地'],
+    properties: {
+      type: 'sword',
+      value: 4,
+    },
+  },
+  {
+    id: '317',
+    category: 'weapons',
+    name: 'トゲモリブリン槍',
+    description:
+      '角を持った動物の骨を槍頭としてくくりつけたモリブリンの槍。雑な作りだが鋭い角の刺突はあまり馬鹿に出来ない貫通力を持つ。',
+    locations: ['ゲルド高地', 'アッカレ高原'],
+    properties: {
+      type: 'sword',
+      value: 9,
+    },
+  },
+  {
+    id: '318',
+    category: 'weapons',
+    name: '竜骨モリブリン槍',
+    description:
+      '戦い慣れたモリブリンが愛用する槍。硬い化石の骨が穂先にいくつも使われている。骨にあるトゲが刺突の威力を高くしている。',
+    locations: ['へブラ山脈', 'オルディン山脈'],
+    properties: {
+      type: 'sword',
+      value: 15,
+    },
+  },
+  {
+    id: '319',
+    category: 'weapons',
+    name: 'リザルスピア',
+    description:
+      '一般的なリザルフォスが装備する槍。波打つ刃を持つ槍頭が特徴的。あまり頑丈ではないので長く使うには心もとない。',
+    locations: ['ラネール湿原', '東ハテール'],
+    properties: {
+      type: 'sword',
+      value: 7,
+    },
+  },
+  {
+    id: '320',
+    category: 'weapons',
+    name: '強化リザルスピア',
+    description:
+      '漁師のモリのような槍頭を持つリザルフォスの槍。実際に魚を獲るのにも使っているらしい。鋭いかえしには注意が必要。',
+    locations: ['タバンタ辺境', 'ゲルド高地'],
+    properties: {
+      type: 'sword',
+      value: 12,
+    },
+  },
+  {
+    id: '321',
+    category: 'weapons',
+    name: '双頭リザルスピア',
+    description:
+      'リザルフォスの中でも戦いを得意とする個体が好んで装備する槍。貫通力はあまり高くないが、波打つ二枚の刃から受ける裂傷は痛手となる。',
+    locations: ['オルディン峡谷', 'アッカレ高原'],
+    properties: {
+      type: 'sword',
+      value: 18,
+    },
+  },
+  {
+    id: '322',
+    category: 'weapons',
+    name: 'ガーディアンランス',
+    description:
+      '小型ガーディアンが装備している槍。構えた時だけ現れる輝く穂先は貫通力が高く、古代シーカー族の技術力の高さがよく分かる。',
+    locations: ['ハイラル平原', 'へブラ山脈'],
+    properties: {
+      type: 'sword',
+      value: 10,
+    },
+  },
+  {
+    id: '323',
+    category: 'weapons',
+    name: 'ガーディアンランス＋',
+    description:
+      '穂先が大きくなったガーディアンランス。狭い場所での取り回しが考慮されているのか、ハイラルの一般的な槍に比べて柄がやや短い。',
+    locations: ['ゲルド高地', 'ハイラル平原'],
+    properties: {
+      type: 'sword',
+      value: 15,
+    },
+  },
+  {
+    id: '324',
+    category: 'weapons',
+    name: 'ガーディアンランス＋＋',
+    description:
+      '最大限まで出力が強化されたガーディアンランス。古代技術で作られた槍頭は刺突に特化しており、大抵の防具ならば易々と貫く高い貫通力を持つ。',
+    locations: ['へブラ山脈', 'アッカレ高原'],
+    properties: {
+      type: 'sword',
+      value: 20,
+    },
+  },
+  {
+    id: '325',
+    category: 'weapons',
+    name: 'ライネルの槍',
+    description:
+      '巨大な曲刀のような穂先を持つライネルの槍。先端が重くバランスが悪いので扱いが難しいが、ライネルはこれを片手で易々と振るう。',
+    locations: ['ゲルド高地', '西ハテール'],
+    properties: {
+      type: 'sword',
+      value: 14,
+    },
+  },
+  {
+    id: '326',
+    category: 'weapons',
+    name: '獣王の槍',
+    description:
+      '斬れ味、重量共に高められたライネルの槍。普通の人間では持ち上げるのさえ一苦労するが、ライネルが振ると岩さえ斬り砕くと言われている。',
+    locations: ['ハイラル大森林', '奥アッカレ'],
+    properties: {
+      type: 'sword',
+      value: 20,
+    },
+  },
+  {
+    id: '327',
+    category: 'weapons',
+    name: '獣神の槍',
+    description:
+      '白髪のライネルが愛用すると言われる槍。斧のような穂先を持ち、その規格外の重さを活かした形で鼻垂れる刺突の破壊力が絶大。',
+    locations: ['ゲルド高地', 'へブラ山脈'],
+    properties: {
+      type: 'sword',
+      value: 30,
+    },
+  },
 ];
