a19c4e01510fda339aa3200257a190c0064048ed
YutaGoto <you.goto.510@gmail.com>
2023-08-24 11:38:55 +0900

    :bento: add shields

--- lib/bowsArrows.ts
+++ lib/bowsArrows.ts
@@ -17,161 +17,176 @@ export const bowsArrows: Item[] = [
     id: '329',
     category: 'bowsArrows',
     name: '木の弓',
-    description: '小型の動物を狩るために作られた木製の弓。魔物と戦うには心もとない。',
+    description:
+      '小型の動物を狩るために作られた木製の弓。魔物と戦うには心もとない。',
     locations: ['ハイラル平原', '西ハテール'],
     properties: {
       type: 'bow',
       value: 4,
-    }
+    },
   },
   {
     id: '330',
     category: 'bowsArrows',
     name: '旅人の弓',
-    description: '旅人が使う護身用の小振りな弓。矢があれば離れた敵を攻撃することができる。',
+    description:
+      '旅人が使う護身用の小振りな弓。矢があれば離れた敵を攻撃することができる。',
     locations: ['ハイラル平原', '西ハテール'],
     properties: {
       type: 'bow',
       value: 5,
-    }
+    },
   },
   {
     id: '331',
     category: 'bowsArrows',
     name: '兵士の弓',
-    description: '戦闘用に攻撃力を高めた弓。より大きなダメージを与えることができる。木で作られているので、火が付くと燃えてしまう。',
+    description:
+      '戦闘用に攻撃力を高めた弓。より大きなダメージを与えることができる。木で作られているので、火が付くと燃えてしまう。',
     locations: ['ハイラル平原', 'フィローネ草原'],
     properties: {
       type: 'bow',
       value: 14,
-    }
+    },
   },
   {
     id: '332',
     category: 'bowsArrows',
     name: '騎士の弓',
-    description: 'ハイラル城の騎士たちが使っていた弓。クセがなくて使いやすく、安定した射撃ができる。金属製なので耐久性も優れている。',
+    description:
+      'ハイラル城の騎士たちが使っていた弓。クセがなくて使いやすく、安定した射撃ができる。金属製なので耐久性も優れている。',
     locations: ['ゲルド砂漠', 'タバンタ辺境'],
     properties: {
       type: 'bow',
       value: 26,
-    }
+    },
   },
   {
     id: '333',
     category: 'bowsArrows',
     name: '王家の弓',
-    description: 'ハイラルの騎士の中でも特に弓術に秀でたものに対して、ハイラル王から与えられた弓。豪奢な意匠が目を引くが射撃性能も優れている。',
+    description:
+      'ハイラルの騎士の中でも特に弓術に秀でたものに対して、ハイラル王から与えられた弓。豪奢な意匠が目を引くが射撃性能も優れている。',
     locations: ['タバンタ辺境', 'へブラ山脈'],
     properties: {
       type: 'bow',
       value: 38,
-    }
+    },
   },
   {
     id: '334',
     category: 'bowsArrows',
     name: '森人の弓',
-    description: 'コログ族が人間のために作ったといわれる弓。良くしなる木材と丈夫な草のツルで作られており、簡素な見た目に反して一度に複数射る事ができる。',
+    description:
+      'コログ族が人間のために作ったといわれる弓。良くしなる木材と丈夫な草のツルで作られており、簡素な見た目に反して一度に複数射る事ができる。',
     locations: ['タバンタ辺境', 'ハイラル丘陵'],
     properties: {
       type: 'bow',
       value: 15,
-    }
+    },
   },
   {
     id: '335',
     category: 'bowsArrows',
     name: 'ゾーラの弓',
-    description: 'ゾーラ族が漁などに使う弓。射撃性能はそれほど高くないが特殊な金属で作られており、耐久性は見た目より優れている。',
+    description:
+      'ゾーラ族が漁などに使う弓。射撃性能はそれほど高くないが特殊な金属で作られており、耐久性は見た目より優れている。',
     locations: ['らねーり大水源', 'アッカレ高原'],
     properties: {
       type: 'bow',
       value: 15,
-    }
+    },
   },
   {
     id: '336',
     category: 'bowsArrows',
     name: 'ツバメの弓',
-    description: 'リト族で一般的に使われている弓。空中での射撃に適した工夫が弦にされており、普通の弓より少し早く引き絞ることができる。',
+    description:
+      'リト族で一般的に使われている弓。空中での射撃に適した工夫が弦にされており、普通の弓より少し早く引き絞ることができる。',
     locations: ['タバンタ辺境', 'へブラ山脈'],
     properties: {
       type: 'bow',
       value: 9,
-    }
+    },
   },
   {
     id: '337',
     category: 'bowsArrows',
     name: 'ハヤブサの弓',
-    description: 'リト族の名工が作り上げた洗練された弓。空の戦いに秀でるリトの戦士が好んで使っている。一般的な弓より素早く引き絞ることができる。',
+    description:
+      'リト族の名工が作り上げた洗練された弓。空の戦いに秀でるリトの戦士が好んで使っている。一般的な弓より素早く引き絞ることができる。',
     locations: ['タバンタ辺境', 'へブラ山脈'],
     properties: {
       type: 'bow',
       value: 20,
-    }
+    },
   },
   {
     id: '338',
     category: 'bowsArrows',
     name: 'オオワシの弓',
-    description: '英傑リーバルが携えていた稀代の名弓。疾風の如き速さでこの弓を射るリーバルに、空中戦で敵う者はいなかったと言われている。',
+    description:
+      '英傑リーバルが携えていた稀代の名弓。疾風の如き速さでこの弓を射るリーバルに、空中戦で敵う者はいなかったと言われている。',
     locations: ['タバンタ辺境'],
     properties: {
       type: 'bow',
-      value: 28
-    }
+      value: 28,
+    },
   },
   {
     id: '339',
     category: 'bowsArrows',
     name: 'ゲルドの弓',
-    description: 'ゲルド族が使う繊細な装飾が施された弓。広い砂漠での狩りや戦闘の際に、遠くの物を狙い撃つ事ができるよう工夫が施されている。',
+    description:
+      'ゲルド族が使う繊細な装飾が施された弓。広い砂漠での狩りや戦闘の際に、遠くの物を狙い撃つ事ができるよう工夫が施されている。',
     locations: ['ゲルド高地', 'ゲルド砂漠'],
     properties: {
       type: 'bow',
       value: 14,
-    }
+    },
   },
   {
     id: '340',
     category: 'bowsArrows',
     name: '一心の弓',
-    description: 'シーカー族に伝わる弓。精神統一をしてこの弓を引くと、遠くの敵がまるで近くにいるかのように狙い撃つ事ができると言われている。',
+    description:
+      'シーカー族に伝わる弓。精神統一をしてこの弓を引くと、遠くの敵がまるで近くにいるかのように狙い撃つ事ができると言われている。',
     locations: ['西ハテール', 'ハイリア湖'],
     properties: {
       type: 'bow',
       value: 10,
-    }
+    },
   },
   {
     id: '341',
     category: 'bowsArrows',
     name: '古代兵装・弓',
-    description: 'ロベリーの研究成果が活かされた高性能な弓。古代シーカー族の技術を利用して、直感的な軌道で矢を放つことができる。',
+    description:
+      'ロベリーの研究成果が活かされた高性能な弓。古代シーカー族の技術を利用して、直感的な軌道で矢を放つことができる。',
     locations: ['不明'],
     properties: {
       type: 'bow',
       value: 44,
-    }
+    },
   },
   {
     id: '342',
     category: 'bowsArrows',
     name: '近衛の弓',
-    description: '大厄災への対策としてシーカー族が作った弓。古代技術を使った試作的な武器であるため、高い射撃力と連射性を持つが耐久力が低くなっている。',
+    description:
+      '大厄災への対策としてシーカー族が作った弓。古代技術を使った試作的な武器であるため、高い射撃力と連射性を持つが耐久力が低くなっている。',
     locations: ['不明'],
     properties: {
       type: 'bow',
       value: 50,
-    }
+    },
   },
   {
     id: '343',
     category: 'bowsArrows',
     name: 'ボコ弓',
-    description: 'ボコブリンが使う簡素な弓。適当な木の枝に弦を張っただけなので、射撃の威力はあまり期待できない。',
+    description:
+      'ボコブリンが使う簡素な弓。適当な木の枝に弦を張っただけなので、射撃の威力はあまり期待できない。',
     locations: ['ハイラル平原', '西ハテール'],
     properties: {
       type: 'bow',
@@ -182,40 +197,44 @@ export const bowsArrows: Item[] = [
     id: '344',
     category: 'bowsArrows',
     name: 'トゲボコ弓',
-    description: '動物の骨をくくりつけて強化されたボコ弓。乱暴な作りではあるが軽くて扱いやすい。耐久性が増しており射撃の威力も上がった。',
+    description:
+      '動物の骨をくくりつけて強化されたボコ弓。乱暴な作りではあるが軽くて扱いやすい。耐久性が増しており射撃の威力も上がった。',
     locations: ['フィローネ草原', '東ハテール'],
     properties: {
       type: 'bow',
       value: 12,
-    }
+    },
   },
   {
     id: '345',
     category: 'bowsArrows',
     name: '竜骨ボコ弓',
-    description: '化石の骨を組み合わせて強化されたボコ弓。ボコブリンなりに厳選した素材で作られているらしく、中々の射撃性能を持つ。',
+    description:
+      '化石の骨を組み合わせて強化されたボコ弓。ボコブリンなりに厳選した素材で作られているらしく、中々の射撃性能を持つ。',
     locations: ['ハイラル丘陵', 'ハイラル平原'],
     properties: {
       type: 'bow',
       value: 24,
-    }
+    },
   },
   {
     id: '346',
     category: 'bowsArrows',
     name: 'リザルボウ',
-    description: 'リザルフォスが作った木製の弓。大型の魚の骨を組み合わせて作られており、単純な木製の弓よりも破壊力がある。',
+    description:
+      'リザルフォスが作った木製の弓。大型の魚の骨を組み合わせて作られており、単純な木製の弓よりも破壊力がある。',
     locations: ['ラネール大水源', 'ラネール湿原'],
     properties: {
       type: 'bow',
       value: 14,
-    }
+    },
   },
   {
     id: '347',
     category: 'bowsArrows',
     name: '強化リザルボウ',
-    description: '金属で持ち手を強化されたリザルボウ。弓本体には水辺に生えたよくしなる木の枝を使っており、あなどれない射撃性能を持つ。',
+    description:
+      '金属で持ち手を強化されたリザルボウ。弓本体には水辺に生えたよくしなる木の枝を使っており、あなどれない射撃性能を持つ。',
     locations: ['タバンタ辺境', 'ゲルド砂漠'],
     properties: {
       type: 'bow',
@@ -226,56 +245,61 @@ export const bowsArrows: Item[] = [
     id: '348',
     category: 'bowsArrows',
     name: '銅鉄リザルボウ',
-    description: 'リザルフォスの中でも弓の扱いに長ける個体のみが持つ弓。強化のために金属が多く使われており、重量があるが耐久性も高め。',
+    description:
+      'リザルフォスの中でも弓の扱いに長ける個体のみが持つ弓。強化のために金属が多く使われており、重量があるが耐久性も高め。',
     locations: ['へブラ山脈', 'アッカレ高原'],
     properties: {
       type: 'bow',
       value: 36,
-    }
+    },
   },
   {
     id: '349',
     category: 'bowsArrows',
     name: 'ライネルの弓',
-    description: 'ライネルが携える武骨な金属性の弓。複数の矢を同時に打ち出す事ができる。広がる射線は素早く動く敵を射抜くのに最適。',
+    description:
+      'ライネルが携える武骨な金属性の弓。複数の矢を同時に打ち出す事ができる。広がる射線は素早く動く敵を射抜くのに最適。',
     locations: ['ゲルド高地', 'ラネール大水源'],
     properties: {
       type: 'bow',
       value: 10,
-    }
+    },
   },
   {
     id: '350',
     category: 'bowsArrows',
     name: '獣王の弓',
-    description: 'より重厚な作りになったライネルの弓。本体だけではなく弦も強靭な金属で作られており、普通の人間にとっては引き絞る事すら難しい。',
+    description:
+      'より重厚な作りになったライネルの弓。本体だけではなく弦も強靭な金属で作られており、普通の人間にとっては引き絞る事すら難しい。',
     locations: ['はいらるへい', '奥アッカレ'],
     properties: {
       type: 'bow',
       value: 20,
-    }
+    },
   },
   {
     id: '351',
     category: 'bowsArrows',
     name: '獣神の弓',
-    description: 'デスマウンテン山頂から取れる特殊な金属で作られたライネルの剛弓。複数放たれる矢は凄まじい威力を持ち、分厚い鎧も紙のように貫く。',
+    description:
+      'デスマウンテン山頂から取れる特殊な金属で作られたライネルの剛弓。複数放たれる矢は凄まじい威力を持ち、分厚い鎧も紙のように貫く。',
     locations: ['へブラ山脈', 'ハイラル平原'],
     properties: {
       type: 'bow',
       value: 32,
-    }
+    },
   },
   {
     id: '352',
     category: 'bowsArrows',
     name: '二連弓',
-    description: 'イーガ団の中でも弓術が得意な構成員が使う弓。狙った敵を確実に仕留めるために、一度で2本の矢を撃てるような工夫がされている。',
+    description:
+      'イーガ団の中でも弓術が得意な構成員が使う弓。狙った敵を確実に仕留めるために、一度で2本の矢を撃てるような工夫がされている。',
     locations: ['ゲルド高地'],
     properties: {
       type: 'bow',
       value: 14,
-    }
+    },
   },
   {
     id: '353',
@@ -283,46 +307,51 @@ export const bowsArrows: Item[] = [
     name: '木の矢',
     description: 'もっとも普通に使われている矢。丈夫な木を削って作られている。',
     locations: ['ハイラル平原', 'オルディン峡谷'],
-    additionalEffect: '特になし'
+    additionalEffect: '特になし',
   },
   {
     id: '354',
     category: 'bowsArrows',
     name: '炎の矢',
-    description: '炎の力が封じられた矢。当たると砕け散り周囲を燃やすことができる。特に冷たいものには絶大な威力がある。',
+    description:
+      '炎の力が封じられた矢。当たると砕け散り周囲を燃やすことができる。特に冷たいものには絶大な威力がある。',
     locations: ['ハイラル平原', 'オルディン峡谷'],
-    additionalEffect: '炎症による追加ダメージ'
+    additionalEffect: '炎症による追加ダメージ',
   },
   {
     id: '355',
     category: 'bowsArrows',
     name: '氷の矢',
-    description: '氷の力が封じられた矢。当たると砕け散り周囲を凍らせることができる。特に熱いものには絶大な威力がある。',
+    description:
+      '氷の力が封じられた矢。当たると砕け散り周囲を凍らせることができる。特に熱いものには絶大な威力がある。',
     locations: ['ハイラル平原', 'へブラ山脈'],
-    additionalEffect: '凍結による行動停止'
+    additionalEffect: '凍結による行動停止',
   },
   {
     id: '356',
     category: 'bowsArrows',
     name: '電気の矢',
-    description: '電気の力が封じられた矢。当たると砕け散り周囲に電気が発生する。特に金属製や水に濡れたものには効果が大きい。',
+    description:
+      '電気の力が封じられた矢。当たると砕け散り周囲に電気が発生する。特に金属製や水に濡れたものには効果が大きい。',
     locations: ['ラネール大水源', 'ハイラル平原'],
-    additionalEffect: '電撃による武器解除'
+    additionalEffect: '電撃による武器解除',
   },
   {
     id: '357',
     category: 'bowsArrows',
     name: 'バクダン矢',
-    description: '魔物に対抗するために作り出された強力な矢。先端に爆薬が仕込んであり、着弾すると爆発して周囲にも大きなダメージを与える。',
+    description:
+      '魔物に対抗するために作り出された強力な矢。先端に爆薬が仕込んであり、着弾すると爆発して周囲にも大きなダメージを与える。',
     locations: ['ハイラル平原', '東ハテール'],
-    additionalEffect: '爆発による大ダメージ'
+    additionalEffect: '爆発による大ダメージ',
   },
   {
     id: '358',
     category: 'bowsArrows',
     name: '古代兵装・矢',
-    description: '古の技術が使われた短剣状の刃を矢尻に転用した武器。古代エネルギーの力で敵を消滅させる。ガーディアンに対しても高い威力を発揮する。',
+    description:
+      '古の技術が使われた短剣状の刃を矢尻に転用した武器。古代エネルギーの力で敵を消滅させる。ガーディアンに対しても高い威力を発揮する。',
     locations: ['ハイラル平原'],
-    additionalEffect: '古代の力による特大ダメージ'
-  }
+    additionalEffect: '古代の力による特大ダメージ',
+  },
 ];
