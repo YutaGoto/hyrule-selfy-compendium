32f5f5ffd12c66a97e2a03125103e9147d6ed522
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-01-04 13:02:27 +0900

    :wrench: add chromatic action

--- ui/Tag.tsx
+++ ui/Tag.tsx
@@ -4,7 +4,9 @@ interface TagProps {
 
 export const Tag = ({ text }: TagProps) => (
   <span
-    className={"bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300"}
+    className={
+      'bg-indigo-100 text-indigo-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-indigo-900 dark:text-indigo-300'
+    }
   >
     {text}
   </span>
