b3d4e99d656ff1886fb7996947692d779ee23106
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-01-04 12:28:56 +0900

    :book: add some stories

---
+++ stories/Button.stories.tsx
@@ -0,0 +1,24 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Button } from '@/ui/Button';
+
+const meta: Meta<typeof Button> = {
+  title: 'Button',
+  component: Button,
+  argTypes: {
+    children: {
+      control: 'text',
+    },
+    href: {
+      control: 'text',
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    children: 'Button',
+  },
+};
---
+++ stories/Clickable.stories.tsx
@@ -0,0 +1,32 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Clickable } from '@/ui/Clickable';
+
+const meta: Meta<typeof Clickable> = {
+  title: 'Clickable',
+  component: Clickable,
+  argTypes: {
+    children: {
+      control: 'text',
+    },
+    onClick: {
+      action: 'clicked',
+    },
+    buttonType: {
+      control: {
+        type: 'select',
+        options: ['button', 'submit', 'reset'],
+      },
+    },
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof Clickable>;
+
+export const Default: Story = {
+  args: {
+    children: 'Clickable',
+    buttonType: 'button',
+    onClick: () => {},
+  },
+};
