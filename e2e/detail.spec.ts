480e7a2bc28cbd243ed268acd36141e8d560ced7
YutaGoto <12590762+YutaGoto@users.noreply.github.com>
2024-05-16 11:06:23 +0900

    :white_check_mark: add detail page e2e testing

---
+++ e2e/detail.spec.ts
@@ -0,0 +1,21 @@
+import { expect, test } from '@chromatic-com/playwright';
+
+test.describe('detail page', async () => {
+  test('should have the correct title', async ({ page }) => {
+    await page.goto('/010');
+
+    await expect(page).toHaveTitle('モリイノシシ - 自撮りハイラル図鑑');
+  });
+
+  test('show the correct item name and ID', async ({page}) => {
+    await page.goto('/010');
+
+    await expect(
+      page.getByText('モリイノシシ').first()
+    ).toBeVisible();
+
+    await expect(
+      page.getByText('No. 010').first()
+    ).toBeVisible();
+  })
+})
